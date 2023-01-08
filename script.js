b8dafadcaede7119427f1930d6a34d2fb380cb97
Dylen <dylenbelanger2004@gmail.com>
2023-01-07 20:55:38 -0500

    Add getPlayerChoice function, create function to captialize first character, and a function that plays one round

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-const choices = ["rock", "paper", "scissors"]
-
 function getComputerChoice() {
     let choiceIndex = Math.floor(Math.random() * 3);
     let computerChoice = choices[choiceIndex];
 
     return computerChoice;
 }
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Choose Rock, Paper, Scissors");
+
+    return playerChoice;
+}
+
+function captializeFirstChar(string) {
+    let firstChar = string.charAt(0);
+    let remainingString = string.slice(1);
+
+    return firstChar.toUpperCase() + remainingString.toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors"
+        || playerSelection === "Paper" && computerSelection === "Rock"
+        || playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+
+    return `You lose! ${computerSelection} beats ${playerSelection}.`;
+
+}
+
+const choices = ["Rock", "Paper", "Scissors"]
+
+let playerSelection = getPlayerChoice()
+playerSelection = captializeFirstChar(playerSelection);
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
 
